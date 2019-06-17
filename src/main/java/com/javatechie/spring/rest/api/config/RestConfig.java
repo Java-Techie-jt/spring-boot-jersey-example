2a77e0ecd6a91b10b18a427cbaf97cd17c01b8ab
Basant kumar Hota <basahota@users.noreply.github.com>
2019-06-17 23:57:56 +0530

    #code

---
+++ src/main/java/com/javatechie/spring/rest/api/config/RestConfig.java
@@ -0,0 +1,17 @@
+package com.javatechie.spring.rest.api.config;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+import com.javatechie.spring.rest.api.resource.EmployeeResource;
+
+@Component
+public class RestConfig extends ResourceConfig{
+
+	public RestConfig(Class<?>... classes) {
+		register(EmployeeResource.class);
+	}
+	
+	
+
+}
